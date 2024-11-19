a0c82914357d534d2558c8b232e850767bcb7e85
yamine59 <yamineizi22@gmail.com>
2024-11-19 12:09:28 +0100

    first commit

---
+++ frontcicd/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <h1>test cicd front et backd</h1>
+      <h2>{{ test }}</h2>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue';
+
+const test = ref(null);
+//fffff
+fetch('http://localhost:3001/users/test', {  // URL complète du backend
+  method: 'GET',
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.coucou); // Affiche "test" dans la console
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête :', error);
+  });
+
+</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
